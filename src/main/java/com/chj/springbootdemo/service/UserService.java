a049e527bf3cff73083254bc0427a89b191524d7
chehaojie <haojayc@163.com>
2019-04-18 11:54:17 +0800

    user service add comment

--- src/main/java/com/chj/springbootdemo/service/UserService.java
+++ src/main/java/com/chj/springbootdemo/service/UserService.java
@@ -8,6 +8,9 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
+/**
+ * @author chehaojie
+ */
 public interface UserService {
 
     List<UserDTO> findAll(Example<User> example);
