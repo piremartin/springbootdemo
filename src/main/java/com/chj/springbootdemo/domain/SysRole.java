f4462f5527d31daf83806d2778b21f95dec80b0d
haojay <haojayc@163.com>
2019-04-21 23:12:08 +0800

    spring security

---
+++ src/main/java/com/chj/springbootdemo/domain/SysRole.java
@@ -0,0 +1,27 @@
+package com.chj.springbootdemo.domain;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * @author chehaojie
+ * @date 2019/04/21 23:02
+ */
+@Data
+@Entity
+public class SysRole {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    /**
+     * 角色名称
+     */
+    @Column(name = "role_name")
+    private String roleName;
+}
---
+++ src/main/java/com/chj/springbootdemo/domain/SysUser.java
@@ -0,0 +1,74 @@
+package com.chj.springbootdemo.domain;
+
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author chehaojie
+ * @date 2019/04/21 23:04
+ */
+@Data
+@Entity
+public class SysUser implements UserDetails {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(cascade = {CascadeType.REFRESH}, fetch = FetchType.EAGER)
+    private List<SysRole> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        List<SysRole> roles = this.getRoles();
+        for (SysRole role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
+        }
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
