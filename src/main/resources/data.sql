647a82053a0b9c8c50eb6d157b51480376502376
haojay <haojayc@163.com>
2019-04-26 15:19:33 +0800

    data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-insert into SYS_USER(id,username,password) value (1, 'wyf', 'wyf123');
-insert into SYS_USER(id,username,password) value (2, 'chj', 'chj123');
+insert into SYS_USER(id,username,password) value (1, 'chj', '123');
+insert into SYS_USER(id,username,password) value (2, 'wyf', '123');
 
-insert into SYS_ROLE(id,role_name) value (1, 'ROLE_ADMIN');
-insert into SYS_ROLE(id,role_name) value (2, 'ROLE_USER');
+insert into SYS_ROLE(id,`name`) value (1, 'ROLE_ADMIN');
+insert into SYS_ROLE(id,`name`) value (2, 'ROLE_USER');
 
 insert into SYS_USER_ROLES(SYS_USER_ID,ROLES_ID) value (1, 1);
 insert into SYS_USER_ROLES(SYS_USER_ID,ROLES_ID) value (2, 2);
