d0cf218ae1ec5a4d4583f083865f92f96ef00e6d
haojay <haojayc@163.com>
2019-05-04 15:49:47 +0800

    junit test

--- src/test/java/com/chj/springbootdemo/web/rest/UserResourceTest.java
+++ src/test/java/com/chj/springbootdemo/web/rest/UserResourceTest.java
@@ -7,13 +7,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.StringUtils;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -32,7 +36,7 @@ public class UserResourceTest {
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Test
-    public void test_batchInsertUser(){
+    public void test_batchInsertUser() {
         LocalDateTime now = LocalDateTime.now();
         List<User> users = new ArrayList<>();
         User user = new User();
@@ -54,7 +58,7 @@ public class UserResourceTest {
     }
 
     @Test
-    public void test_addUser(){
+    public void test_addUser_use_map() {
         String sql = "insert into User(`name`, age, create_time) " +
                 "values('lucas', 18, :createTime)";
         Map<String, Object> map = new HashMap<>(1);
@@ -63,7 +67,7 @@ public class UserResourceTest {
     }
 
     @Test
-    public void test_queryForObject_exist(){
+    public void test_queryForObject_exist() {
 
         String sql = "select `name` from user where id=1";
         String name = namedParameterJdbcTemplate.queryForObject(sql, new HashMap<>(1), String.class);
@@ -71,7 +75,7 @@ public class UserResourceTest {
     }
 
     @Test
-    public void test_queryForObject_exception_except1_actual0(){
+    public void test_queryForObject_exception_except1_actual0() {
 
         String sql = "select `name` from user where id=0";
         //org.springframework.dao.EmptyResultDataAccessException: Incorrect result size: expected 1, actual 0
@@ -81,7 +85,7 @@ public class UserResourceTest {
     }
 
     @Test
-    public void test_queryForObject_exception_except1_actual2(){
+    public void test_queryForObject_exception_except1_actual2() {
 
         String sql = "select `name` from user where age=18";
         //org.springframework.dao.IncorrectResultSizeDataAccessException: Incorrect result size: expected 1, actual 2
@@ -91,7 +95,38 @@ public class UserResourceTest {
     }
 
     @Test
-    public void test_query1(){
+    public void test_queryForObject_solve_except1_actualX() {
+
+        String sql = "select `name` from user where age=18";
+        String name = namedParameterJdbcTemplate.query(sql, new HashMap<>(1), new ResultSetExtractor<String>() {
+            @Override
+            public String extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+                if (resultSet.next()) {
+                    return resultSet.getString("name");
+                }
+                return null;
+            }
+        });
+        Assert.assertTrue(StringUtils.hasText(name));
+        System.out.println("name=" + name);
+    }
+
+    @Test
+    public void test_queryForObject_solve_except1_actualX_use_lamda() {
+
+        String sql = "select `name` from user where age=18 order by create_time desc ";
+        String name = namedParameterJdbcTemplate.query(sql, new HashMap<>(1), (ResultSet resultSet) -> {
+            if (resultSet.next()) {
+                return resultSet.getString("name");
+            }
+            return null;
+        });
+        Assert.assertTrue(StringUtils.hasText(name));
+        System.out.println("name=" + name);
+    }
+
+    @Test
+    public void test_query1() {
         String sql = "select * from user where name like :name";
         Map<String, Object> map = new HashMap<>();
         map.put("name", "n%");
@@ -100,14 +135,14 @@ public class UserResourceTest {
     }
 
     @Test
-    public void test_query2(){
+    public void test_query2() {
         String sql = "select * from user where id<0";
         List<UserDTO> users = namedParameterJdbcTemplate.query(sql, new BeanPropertyRowMapper<>(UserDTO.class));
         System.out.println(users);
     }
 
     @Test
-    public void test_query3(){
+    public void test_query3() {
         String sql = "select * from user where id < :id";
         Map<String, Object> map = new HashMap<>();
         map.put("id", 3);
@@ -116,23 +151,25 @@ public class UserResourceTest {
     }
 
     @Test
-    public void test_query4(){
+    public void test_query4() {
         String sql = "select `name` from user where id < :id";
         Map<String, Object> map = new HashMap<>();
         map.put("id", 3);
         List<String> names = namedParameterJdbcTemplate.queryForList(sql, map, String.class);
         System.out.println(names);
     }
+
     @Test
-    public void test_query5(){
+    public void test_query5() {
         String sql = "select age from user where id < :id";
         Map<String, Object> map = new HashMap<>();
         map.put("id", 3);
         List<Integer> ages = namedParameterJdbcTemplate.queryForList(sql, map, Integer.class);
         System.out.println(ages);
     }
+
     @Test
-    public void test_query6(){
+    public void test_query6() {
         String sql = "select id from user where id < :id";
         Map<String, Object> map = new HashMap<>();
         map.put("id", 3);
@@ -141,7 +178,7 @@ public class UserResourceTest {
     }
 
     @Test
-    public void test_insert(){
+    public void test_insert() {
         Long id = 8L;
         int age = 22;
         for (int i = 0; i < 3; i++) {
@@ -157,7 +194,7 @@ public class UserResourceTest {
     }
 
     @Test
-    public void test_batch_insert(){
+    public void test_batch_insert() {
         List<User> users = new ArrayList<>();
 
         Long id = 10031L;
@@ -179,11 +216,11 @@ public class UserResourceTest {
         String sql = "insert into user(id, `name`, age, create_time) value (:id, :name, :age, :createTime)";
         SqlParameterSource[] batch = SqlParameterSourceUtils.createBatch(users.toArray());
         long start = System.currentTimeMillis();
-        System.out.println("start:"+start);
+        System.out.println("start:" + start);
         namedParameterJdbcTemplate.batchUpdate(sql, batch);
         long end = System.currentTimeMillis();
-        System.out.println("end:"+end);
-        System.out.println("cost millis:"+(end - start));
+        System.out.println("end:" + end);
+        System.out.println("cost millis:" + (end - start));
 
     }
 }
