a61344f254f43faeb13eba989c2237465e551d86
haojay <haojayc@163.com>
2019-05-04 15:25:45 +0800

    auto increase

--- src/main/java/com/chj/springbootdemo/domain/User.java
+++ src/main/java/com/chj/springbootdemo/domain/User.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 public class User implements Serializable {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
